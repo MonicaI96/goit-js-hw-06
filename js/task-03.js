1509931193fd1c421fbf07c1de5ed20ac1b965b7
MonicaI96 <monyca.stan@yahoo.com>
2023-11-16 14:29:31 +0000

    Done

--- js/task-03.js
+++ js/task-03.js
@@ -3,7 +3,9 @@ const images = [
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'White and Black Long Fur Cat',
   },
+
   {
+   
     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
